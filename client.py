09c83e1d30da9154178b50e9580a9286b254f1ad
ksv <vidyullatha.k-s@hpe.com>
2020-08-03 00:24:05 +0530

    commit

---
+++ client.py
@@ -0,0 +1,66 @@
+# Socket Chatroom client - Creating chat application with sockets in Python
+import socket
+import select
+import errno
+HEADER_LENGTH = 10
+IP = "127.0.0.1"
+PORT = 3000
+my_username = input("Username: ")
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+# Prepare username and header and send them
+# We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
+username = my_username.encode('utf-8')
+urs_header = "{len(username):<{HEADER_LENGTH}}"
+username_header = urs_header.encode('utf-8')
+client_socket.send(username_header + username)
+
+while True:
+    # Wait for user to input a message
+    message = input('{} > '.format(my_username))
+    # If message is not empty - send it
+    if message:
+        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+        message = message.encode('utf-8')
+        msg_header = "{len(message):<{HEADER_LENGTH}}"
+        message_header = msg_header.encode('utf-8')
+        client_socket.send(message_header + message)
+
+    try:
+        # Now we want to loop over received messages (there might be more than one) and print them
+        while True:
+
+            # Receive our "header" containing username length, it's size is defined and constant
+            username_header = client_socket.recv(HEADER_LENGTH)
+
+            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+            if not len(username_header):
+                print('Connection closed by the server')
+                sys.exit()
+
+            # Convert header to int value
+            username_length = int(username_header.decode('utf-8').strip())
+            # Receive and decode username
+            username = client_socket.recv(username_length).decode('utf-8')
+
+            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
+
+            print('{} > {}'.format(username, message))
+
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            sys.exit()
+        # We just did not receive anything
+        continue
+
+    except Exception as e:
+        # Any other exception - something happened, exit
+        print('Reading error: '.format(str(e)))
+        sys.exit()
